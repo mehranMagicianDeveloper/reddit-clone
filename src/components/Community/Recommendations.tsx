53352013f8ac2dd2e78a5b7ebbc1a78b57cacb06
Mehran Hamzei <mehran.magicianDeveloper@gmail.com>
2024-01-28 10:16:58 +0330

    update community recommendations

--- src/components/Community/Recommendations.tsx
+++ src/components/Community/Recommendations.tsx
@@ -2,8 +2,7 @@ import { Community } from "@/src/atoms/communitiesAtom";
 import { firestore } from "@/src/firebase/clientApp";
 import useCommunityData from "@/src/hooks/useCommunityData";
 import { Flex } from "@chakra-ui/react";
-import firebase from "firebase/compat/app";
-import { collection, query } from "firebase/firestore";
+import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 
 const Recommendations: React.FC = () => {
@@ -13,7 +12,17 @@ const Recommendations: React.FC = () => {
 
   const getCommunityRecommendations = async () => {
     try {
-      const communitiesQuery = query(collection(firestore, "communities"));
+      const communitiesQuery = query(
+        collection(firestore, "communities"),
+        orderBy("numberOfMembers", "desc"),
+        limit(5)
+      );
+      const communityDocs = await getDocs(communitiesQuery);
+      const communities = communityDocs.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setCommunities(communities as Community[]);
     } catch (error: any) {
       console.log("getCommunityRecommendations error: ", error);
     }
